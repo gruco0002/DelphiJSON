f3a67b200090bcb034bdbbe13f84d8041d796236
gruco0002 <corbaer@googlemail.com>
2020-09-08 16:00:44 +0200

    added tests for utc settings

--- Tests/DateAndTimeTests.pas
+++ Tests/DateAndTimeTests.pas
@@ -17,6 +17,12 @@ type
     [Test]
     procedure DateTimeDeserialization;
 
+    [Test]
+    procedure TestUTCSettingsSerialization;
+
+    [Test]
+    procedure TestUTCSettingsDeserialization;
+
   end;
 
 implementation
@@ -55,6 +61,68 @@ begin
   desired.Free;
   ser.Free;
 
+end;
+
+procedure TDateAndTimeTests.TestUTCSettingsDeserialization;
+const
+  res = '"2020-08-23T10:40:42.153-05:00"';
+var
+  dt: TDateTime;
+  settings: TDJSettings;
+  timezone: TTimeZone;
+begin
+  timezone := TTimeZone.Local;
+
+  settings := TDJSettings.Default;
+
+  dt := DelphiJSON<TDateTime>.Deserialize(res);
+  Assert.AreEqual(EncodeDateTime(2020, 8, 23, 15, 40, 42, 153), dt);
+
+  settings.DateTimeReturnUTC := true;
+
+  dt := DelphiJSON<TDateTime>.Deserialize(res, settings);
+  Assert.AreEqual(EncodeDateTime(2020, 8, 23, 15, 40, 42, 153), dt);
+
+  settings.DateTimeReturnUTC := false;
+
+  dt := DelphiJSON<TDateTime>.Deserialize(res, settings);
+  Assert.AreEqual(timezone.ToLocalTime(EncodeDateTime(2020, 8, 23, 15, 40, 42,
+    153)), dt);
+
+  settings.Free;
+
+end;
+
+procedure TDateAndTimeTests.TestUTCSettingsSerialization;
+var
+  dt: TDateTime;
+  ser: TJSONValue;
+  desired: TJSONValue;
+  settings: TDJSettings;
+begin
+  dt := EncodeDateTime(2020, 4, 23, 10, 12, 11, 154);
+
+  settings := TDJSettings.Default;
+  settings.DateTimeReturnUTC := false;
+
+  ser := DelphiJSON<TDateTime>.SerializeJ(dt, settings);
+  desired := TJSONString.Create(DateToISO8601(dt, false));
+  Assert.IsTrue(JSONEquals(ser, desired));
+
+  desired.Free;
+  ser.Free;
+  desired := nil;
+  ser := nil;
+
+  settings.DateTimeReturnUTC := true;
+  ser := DelphiJSON<TDateTime>.SerializeJ(dt, settings);
+  desired := TJSONString.Create(DateToISO8601(dt, true));
+  Assert.IsTrue(JSONEquals(ser, desired));
+
+  desired.Free;
+  ser.Free;
+
+  settings.Free;
 end;
 
 initialization
