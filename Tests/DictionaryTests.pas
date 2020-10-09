c5f3d1fa58237c0fcf56ad7fc754cba9884b992b
gruco0002 <dev.gruco0002@gmail.com>
2020-10-09 13:06:16 +0200

    added tests for TreatStringDictionaryAsObject, closes #12

--- Tests/DictionaryTests.pas
+++ Tests/DictionaryTests.pas
@@ -23,6 +23,12 @@ type
     [Test]
     procedure TestDerStringKey;
 
+    [Test]
+    procedure TestSerStringKeyDisabled;
+
+    [Test]
+    procedure TestDerStringKeyDisabled;
+
   end;
 
 implementation
@@ -50,6 +56,40 @@ begin
   Assert.AreEqual(32, dict['Matrix']);
 
   dict.Free;
+end;
+
+procedure TDictionaryTests.TestDerStringKeyDisabled;
+const
+  res1 = '{"Smith":56,"Neo":42,"Matrix":32}';
+  res = '[{"key": "Smith", "value": 56},{"key": "Neo", "value": 42},{"key": "Matrix", "value": 32}]';
+var
+  dict: TDictionary<String, Integer>;
+  setting: TDJSettings;
+begin
+  setting := TDJSettings.Default;
+  setting.TreatStringDictionaryAsObject := false;
+  Assert.WillRaise(
+    procedure
+    begin
+      dict := DelphiJSON < TDictionary < String,
+        Integer >>.Deserialize(res1, setting);
+    end, EDJError);
+
+  dict := DelphiJSON < TDictionary < String,
+    Integer >>.Deserialize(res, setting);
+
+  Assert.AreEqual(3, dict.Count);
+  Assert.IsTrue(dict.ContainsKey('Neo'));
+  Assert.IsTrue(dict.ContainsKey('Smith'));
+  Assert.IsTrue(dict.ContainsKey('Matrix'));
+
+  Assert.AreEqual(56, dict['Smith']);
+  Assert.AreEqual(42, dict['Neo']);
+  Assert.AreEqual(32, dict['Matrix']);
+
+  dict.Free;
+  setting.Free;
+
 end;
 
 procedure TDictionaryTests.TestDeserialization;
@@ -120,6 +160,35 @@ begin
   serialized.Free;
   desired.Free;
   dict.Free;
+end;
+
+procedure TDictionaryTests.TestSerStringKeyDisabled;
+const
+  res = '[{"key": "Smith", "value": 56},{"key": "Neo", "value": 42},{"key": "Matrix", "value": 32}]';
+var
+  dict: TDictionary<String, Integer>;
+  desired: TJSONValue;
+  serialized: TJSONValue;
+  setting: TDJSettings;
+begin
+  setting := TDJSettings.Default;
+  setting.TreatStringDictionaryAsObject := false;
+  dict := TDictionary<String, Integer>.Create;
+  dict.Add('Smith', 56);
+  dict.Add('Neo', 42);
+  dict.Add('Matrix', 32);
+
+  desired := TJSONObject.ParseJSONValue(res);
+
+  serialized := DelphiJSON < TDictionary < String,
+    Integer >>.SerializeJ(dict, setting);
+
+  Assert.IsTrue(JSONEquals(desired, serialized, false));
+
+  serialized.Free;
+  desired.Free;
+  dict.Free;
+  setting.Free;
 end;
 
 initialization
